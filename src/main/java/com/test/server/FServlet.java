fd1325a97898e1b7608a1a7394058ef6b865dce3
zy <814575934@qq.com>
2019-10-08 20:35:07 +0800

    改了下目录结构

--- src/main/java/com/test/server/FServlet.java
+++ src/main/java/com/test/server/FServlet.java
@@ -7,20 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "TServlet",urlPatterns = "/t")
-public class TServlet extends HttpServlet {
+@WebServlet(name = "FServlet")
+public class FServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name1 = request.getParameter("name");
-        String age = request.getParameter("age");
-        System.out.println("POST请求成功了"+name1+age);
-//        doGet(request,response);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html ; charset =UTF-8");
 
-        String name1 = request.getParameter("name");
-        String age = request.getParameter("age");
-        System.out.println("get请求成功了"+name1+age);
     }
 }
